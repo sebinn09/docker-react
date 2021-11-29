45653b40140cf3bdb7b30cb450dad5668f796ad8
root <root@ip-172-31-10-195.ap-south-1.compute.internal>
2021-11-29 11:50:30 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          BYE BYE..............
         </a>
       </header>
     </div>
